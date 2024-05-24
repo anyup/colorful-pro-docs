aaa570dd0c79d1102eb84a13c9a08d7a67b17153
qiaomingxing <491302297@qq.com>
2024-05-24 15:41:02 +0800

    feat: uni-app 专栏

--- docs/.vitepress/config/menu.ts
+++ docs/.vitepress/config/menu.ts
@@ -35,7 +35,8 @@ class Menu<T> {
         activeMatch: `/${this.locale}/blog/`,
         items: [
           { text: '关于我', link: `/${this.locale}/blog/` },
-          { text: '可视化大屏', link: `/${this.locale}/blog/large-screen/`, activeMatch: `/${this.locale}/blog/large-screen/` }
+          { text: '可视化大屏', link: `/${this.locale}/blog/large-screen/`, activeMatch: `/${this.locale}/blog/large-screen/` },
+          { text: 'uni-app开发', link: `/${this.locale}/blog/uni-app/`, activeMatch: `/${this.locale}/blog/uni-app/` }
         ]
       }
     ]
@@ -55,13 +56,32 @@ class Menu<T> {
     return [
       {
         text: '可视化大屏',
-        collapsed: false,
+        collapsed: true,
         items: addTextIndex([
+          { text: '专栏简介 - 可视化大屏', link: 'large-screen/' },
           { text: 'ECharts十万级+数据渲染性能优化方案', link: 'large-screen/ECharts十万级+数据渲染性能优化方案' },
           { text: 'ECharts地图实战分析：实现一个完整的地图下钻功能', link: 'large-screen/ECharts地图实战分析：实现一个完整的地图下钻功能' },
           { text: '产品需求：ECharts鼠标自由刷选区域，定向放大图表', link: 'large-screen/产品需求：ECharts鼠标自由刷选区域，定向放大图表' },
           { text: '分享ECharts地图合规整改经验，并实现一个最基础的中国地图', link: 'large-screen/分享ECharts地图合规整改经验，并实现一个最基础的中国地图' },
           { text: '分析ECharts图表渲染导致的内存泄漏问题-附解决方案', link: 'large-screen/分析ECharts图表渲染导致的内存泄漏问题-附解决方案' }
+        ])
+      },
+      {
+        text: 'uni-app开发',
+        collapsed: true,
+        items: addTextIndex([
+          { text: '专栏简介 - uni-app开发', link: 'uni-app/' },
+          { text: '解析 uni-app 小程序分包策略：合理优化包体积分布', link: 'uni-app/解析 uni-app 小程序分包策略：合理优化包体积分布' },
+          { text: '【超详细】从0到1打包你的uni-app应用：安卓篇打包指南', link: 'uni-app/【超详细】从0到1打包你的uni-app应用：安卓篇打包指南' },
+          { text: 'uni-app跳坑系列：谈谈我是如何提升安卓App扫码准确率的', link: 'uni-app/uni-app跳坑系列：谈谈我是如何提升安卓App扫码准确率的' },
+          { text: 'uni-app扫码插件推荐：基于支付宝mPaaS扫码组件进行开发', link: 'uni-app/uni-app扫码插件推荐：基于支付宝mPaaS扫码组件进行开发' },
+          { text: 'uni-app图标库整合：使用iconfont构建属于自己的图标库', link: 'uni-app/uni-app图标库整合：使用iconfont构建属于自己的图标库' },
+          { text: '一文读懂uni-app中的尺寸单位rpx', link: 'uni-app/一文读懂uni-app中的尺寸单位rpx' },
+          { text: '玩转uni-app多端部署：自定义平台的条件编译实战详解', link: 'uni-app/玩转uni-app多端部署：自定义平台的条件编译实战详解' },
+          { text: '玩转uni-app多端编译：static目录进行条件编译的最佳实践', link: 'uni-app/玩转uni-app多端编译：static目录进行条件编译的最佳实践' },
+          { text: '经历了PMP和软考高项，我独立开发了一款刷题小程序', link: 'uni-app/经历了PMP和软考高项，我独立开发了一款刷题小程序' },
+          { text: '100行代码打造小而美的uni-app请求库', link: 'uni-app/100行代码打造小而美的uni-app请求库' },
+          { text: '前端小工具！加速构建你的API请求类，全端通用', link: 'uni-app/前端小工具！加速构建你的API请求类，全端通用' }
         ])
       }
     ]
