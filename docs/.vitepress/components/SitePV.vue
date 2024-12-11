42e53762f677fd8395cd3c1f2338c1215ce96133
qiaomingxing <491302297@qq.com>
2024-12-11 20:05:47 +0800

    feat: 不蒜子更新

--- docs/.vitepress/components/SitePV.vue
+++ docs/.vitepress/components/SitePV.vue
@@ -6,7 +6,40 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const offset = {
+  sitePV: 10000,
+  siteUV: 5000,
+  pagePV: 2000
+}
+const setValue = (id: string, offset: number) => {
+  // 获取页面元素
+  const dom = document.getElementById(id)
+  // 检查是否成功获取到了pv元素，并且它有内容（即不蒜子已经加载并设置了值）
+  if (dom && dom.innerHTML) {
+    // 将文本内容转换为整数并加上偏移量
+    var currentPV = parseInt(dom.innerHTML, 10)
+    var newPV = currentPV + offset
+    // 更新页面上的显示
+    dom.innerHTML = String(newPV)
+    return
+  }
+  // 如果没有成功获取到pv元素，等待1秒后再次尝试
+  setTimeout(() => {
+    setValue(id, offset)
+  }, 1000)
+}
+onMounted(() => {
+  // 检查并更新page pv
+  setValue('busuanzi_value_page_pv', offset.pagePV)
+  // 检查并更新site uv
+  setValue('busuanzi_value_site_uv', offset.siteUV)
+  // 检查并更新site pv
+  setValue('busuanzi_value_site_pv', offset.sitePV)
+})
+</script>
 
 <style scoped>
 .pv-row {
